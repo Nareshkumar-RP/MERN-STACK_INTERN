514b5556c75cf19b50dbd3d1b65e060ecded47cd
Nareshkumar-RP <152085480+Nareshkumar-RP@users.noreply.github.com>
2025-01-23 17:09:57 +0530

    Add files via upload

---
+++ ROUTER/src/components/About.jsx
@@ -0,0 +1,13 @@
+import './About.css';
+const About = () => {
+    return (
+        <div className="about-design">
+            <h1>About Page is Here.....</h1>
+            <p className='about-description'></p>
+            <div className='about-image'>
+                <img src=''></img>
+
+            </div>
+        </div>)
+};
+export default About;
---
+++ ROUTER/src/components/NotFound.jsx
@@ -0,0 +1,4 @@
+const NotFound = () => {
+    return <h1>NotFound Page is Here.....</h1>
+};
+export default NotFound;
